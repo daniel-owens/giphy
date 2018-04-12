44ea493064bcbee291ceeb71bb439bce3b1f7872
Daniel Owens <DanielOwens@Daniel-Owenss-MacBook-Pro-2.local>
2018-04-12 14:51:02 -0700

    changed http to https re queryURL of giphy

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -48,7 +48,7 @@ var sexyNumber = 0;
 
 function displayAnimalInfo() {
 	var animal = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10"; 
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10"; 
 
     $.ajax({
     	url: queryURL,
